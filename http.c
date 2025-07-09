9241b66b6b49a37d4463f7f297ba7f9261c809d8
shegaoyuan@aliyun.com <shegaoyuan@aliyun.com>
2025-07-09 14:25:35 +0800

    opt: set null when connection closed

--- http.c
+++ http.c
@@ -86,6 +86,8 @@ static void http_ev_close_cb(struct mg_connection *c, int ev, void *ev_data) {
 
     free(s);
 
+    c->fn_data = NULL;
+
 }
 
 static void http_alive_handler(struct mg_connection *c, int ev, void *ev_data) {
